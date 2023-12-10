932d51fcbf25129c2be3d88f448144367e55344a
Mohammed Asadullah Sayeed <22dp2000212@ds.study.iitm.ac.in>
2023-12-10 19:49:29 +0530

    BetterRest
    Project 4 files added

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Asad Sayeed on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
